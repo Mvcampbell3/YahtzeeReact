80936f415ba695d43533aa4f098b3468a8e18302
Mvcampbell3 <mvcampbell3@gmail.com>
2019-06-03 18:06:56 -0400

    adding dice images

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import "./header.css";
+import { Link } from "react-router-dom";
 
 const Header = (props) => {
-  return ( 
+  return (
     <header>
-      <h1>This is the Header</h1>
+      <h1>Yahtzee</h1>
+      <div className="headerLinks">
+        <Link to="/">Home</Link>
+        <Link to="/score">Scores</Link>
+      </div>
     </header>
-   );
+  );
 }
- 
+
 export default Header;
