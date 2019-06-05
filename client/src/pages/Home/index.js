b32c9ffb3e97a7fc430a8480262bdb6fce3916c6
Mvcampbell3 <mvcampbell3@gmail.com>
2019-06-04 20:14:11 -0400

    adding in more game logic

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -15,7 +15,7 @@ class Home extends Component {
     dicePics: [blank, dice1, dice2, dice3, dice4, dice5, dice6],
     diceValue: [0, 0, 0, 0, 0],
     diceHold: [false, false, false, false, false],
-    rollCount: 3,
+    rollCount: 13,
     scoring: [
       { score: 0, saved: false, place: 0, click: true, value: 1, name: "Ones", scoreSystem: "number" },
       { score: 0, saved: false, place: 1, click: true, value: 2, name: "Twos", scoreSystem: "number" },
@@ -26,19 +26,21 @@ class Home extends Component {
       { score: 0, saved: false, place: 6, click: false, value: "upperSubTotal", name: "Upper Sub Total", scoreSystem: "totalPart" },
       { score: 0, saved: false, place: 7, click: false, value: "upperBonus", name: "Bonus", scoreSystem: "35" },
       { score: 0, saved: false, place: 8, click: false, value: "upperTotal", name: "Upper Total", scoreSystem: "totalPart" },
-      { score: 0, saved: false, place: 9, click: true, value: "threeKind", name: "Three of a kind", scoreSystem: "total" },
-      { score: 0, saved: false, place: 10, click: true, value: "fourKind", name: "Four of a kind", scoreSystem: "total" },
+      { score: 0, saved: false, place: 9, click: true, value: "threeKind", name: "Three of a kind", scoreSystem: "kind" },
+      { score: 0, saved: false, place: 10, click: true, value: "fourKind", name: "Four of a kind", scoreSystem: "kind" },
       { score: 0, saved: false, place: 11, click: true, value: "fullHouse", name: "Full House", scoreSystem: "fullHouse" },
       { score: 0, saved: false, place: 12, click: true, value: 30, name: "Small Straight", scoreSystem: "straight" },
       { score: 0, saved: false, place: 13, click: true, value: 40, name: "Large Straight", scoreSystem: "straight" },
-      { score: 0, saved: false, place: 14, click: true, value: 50, name: "Yahtzee", scoreSystem: "yahztee" },
+      { score: 0, saved: false, place: 14, click: true, value: 50, name: "Yahtzee", scoreSystem: "yahtzee" },
       { score: 0, saved: false, place: 15, click: true, value: "chance", name: "Chance", scoreSystem: "total" },
       { score: 0, saved: false, place: 16, click: true, value: "bonusYahtzee", name: "Bonus Yahtzee", scoreSystem: "bonusYahtzee" },
       { score: 0, saved: false, place: 17, click: false, value: "lowerTotal", name: "Lower Total", scoreSystem: "totalPart" },
       { score: 0, saved: false, place: 18, click: false, value: "total", name: "Total Score", scoreSystem: "totalAll" }
     ],
     bonusYahtzee: false,
-    firstYahtzee: false
+    firstYahtzee: false,
+    savedYahtzee: false,
+    previousPlace: null
 
   }
 
@@ -78,18 +80,35 @@ class Home extends Component {
 
   testScore = (value, name, scoringSystem, place, saved) => {
     console.log(value, name, scoringSystem, place)
-    // const oldScore = this.state.scoring[place].score;
-    const dice = this.state.diceValue;
-    console.log(dice)
-    // console.log(oldScore)
     const scoring = this.state.scoring;
+    // Reset the scores that are not saved
     scoring.forEach(one => {
       if (one.saved === false && one.click === true) {
         one.score = 0;
       }
     })
+
+    if (scoring[place].saved === true) {
+      console.log("can't do that")
+      return;
+    }
+
+    // Consts that the scoring system will need
     let saveNum = 0;
-    //Switch For Scoring System!!
+    const dice = this.state.diceValue;
+    const totalSum = dice.reduce((amt, tot) => amt + tot);
+    console.log(totalSum)
+    const ones = dice.filter(each => each === 1)
+    const twos = dice.filter(each => each === 2)
+    const threes = dice.filter(each => each === 3)
+    const fours = dice.filter(each => each === 4)
+    const fives = dice.filter(each => each === 5)
+    const sixes = dice.filter(each => each === 6)
+    console.log(ones, twos, threes, fours, fives, sixes)
+    const numbers = [ones, twos, threes, fours, fives, sixes]
+    let scoredYahtzee;
+
+    // Switch For Scoring System!!
     switch (scoringSystem) {
       case "number":
         console.log("this is a number score");
@@ -97,22 +116,79 @@ class Home extends Component {
           const numberValue = dice.filter(each => each === value).reduce((amt, tot) => amt + tot);
           console.log(numberValue);
           saveNum = numberValue;
+          scoredYahtzee = false;
         } else {
           console.log(`There are no ${value}'s`)
         }
         break;
+      case "kind":
+        console.log("This is a kind score");
+        console.log(value);
+        value === "threeKind" ?
+          numbers.forEach(one => {
+            if (one.length >= 3) {
+              saveNum = totalSum;
+              scoredYahtzee = false;
+            }
+          })
+          :
+          numbers.forEach(one => {
+            if (one.length >= 4) {
+              saveNum = totalSum;
+              scoredYahtzee = false;
+            }
+          });
+        break;
       case "total":
         console.log("this is a total score");
         saveNum = dice.reduce((amt, tot) => amt + tot);
+        scoredYahtzee = false;
         break;
       case "fullHouse":
         console.log("this is the full house");
+        const triple = numbers.filter((array) => array.length === 3)
+        const double = numbers.filter((array) => array.length === 2)
+
+        if (triple.length > 0 && double.length > 0) {
+          console.log("fullhouse");
+          saveNum = 25;
+          scoredYahtzee = false;
+        } else {
+          console.log("no fullhouse");
+        }
         break;
       case "straight":
         console.log("this is a straight");
+        if (value === 30) {
+          if ((dice.includes(1) && dice.includes(2) && dice.includes(3) && dice.includes(4)) ||
+            (dice.includes(2) && dice.includes(3) && dice.includes(4) && dice.includes(5)) ||
+            (dice.includes(3) && dice.includes(4) && dice.includes(5) && dice.includes(6))) {
+            console.log("small staight");
+            saveNum = value;
+            scoredYahtzee = false;
+          } else {
+            console.log("not a small staight")
+          }
+        } else {
+          if ((dice.includes(1) && dice.includes(2) && dice.includes(3) && dice.includes(4) && dice.includes(5)) ||
+            (dice.includes(2) && dice.includes(3) && dice.includes(4) && dice.includes(5) && dice.includes(6))) {
+            console.log("large staight");
+            saveNum = value;
+            scoredYahtzee = false;
+          } else {
+            console.log("not a large staight")
+          }
+        }
         break;
       case "yahtzee":
         console.log("this is the yahtzee");
+        const checkYahtzee = dice.filter(each => each === dice[0]);
+        console.log(checkYahtzee.length)
+        if (checkYahtzee.length === 5) {
+          console.log("it's a yahtzee");
+          saveNum = 50;
+          scoredYahtzee = true;
+        }
         break;
       case "bonusYahtzee":
         console.log("this is the bonus yahtzee")
@@ -126,15 +202,32 @@ class Home extends Component {
     // This will probably be in a serpate funciton that adds all of the scores together on save
     this.setState((prevState => {
       prevState.scoring[place].score = saveNum;
+      prevState.previousPlace = place;
+      if (scoredYahtzee && this.state.savedYahtzee === false) {
+        prevState.firstYahtzee = true
+      } else if (!scoredYahtzee && this.state.savedYahtzee === false) {
+        prevState.firstYahtzee = false
+      }
       return prevState;
     }))
+  }
+
+  saveScore = () => {
+    const where = this.state.previousPlace;
+    this.setState((prevState) => {
+      prevState.scoring[where].saved = true;
+      if (prevState.firstYahtzee && !prevState.savedYahtzee) {
+        prevState.savedYahtzee = true;
+      }
+      return prevState;
+    })
   }
 
 
   render() {
 
     return (
-      <div>
+      <div className="container">
         <div className="diceBox">
           <img src={this.state.dicePics[this.state.diceValue[0]]} className="die" alt="die1" />
           <img src={this.state.dicePics[this.state.diceValue[1]]} className="die" alt="die2" />
@@ -151,6 +244,7 @@ class Home extends Component {
         </div>
         <button onClick={this.rollDice}>Roll</button>
         <button onClick={this.resetRound}>Clear</button>
+        <button onClick={this.saveScore}>Save</button>
         <div className="scoringGrid">
           {this.state.scoring.map(score =>
             <div key={score.name} className={score.name === "total" ? "totalScore" : "score"}>
