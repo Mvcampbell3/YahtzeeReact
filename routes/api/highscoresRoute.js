bf6a91aa9acdbf174b3ade9b90a5b0aab8d7045f
Mvcampbell3 <mvcampbell3@gmail.com>
2019-06-12 21:41:46 -0400

    added in delete routes for highscores

--- routes/api/highscoresRoute.js
+++ routes/api/highscoresRoute.js
@@ -34,4 +34,31 @@ router.post("/new", checkAuth, (req, res, next) => {
     })
 })
 
+router.get("/userscores", checkAuth, (req, res, next) => {
+  db.HighScore.find({ userID: req.user.userID })
+    .then(result => res.status(200).json(result))
+    .catch(err => res.status(500).json(err))
+})
+
+router.delete("/delete/:scoreid", checkAuth, (req, res, next) => {
+  const userID = req.user.userID;
+  const scoreID = req.params.scoreid;
+  db.HighScore.findById(scoreID)
+    .then(score => {
+      if (score.userID === userID) {
+        score.remove()
+          .then(deleted => res.status(200).json(deleted))
+          .catch(err => res.status(500).json(err))
+      } else {
+        res.status(401).json("Unauthorized")
+      }
+    })
+})
+
+router.delete("/delallscores/special/all/delete", (req, res, next) => {
+  db.HighScore.remove()
+    .then(result => res.status(200).json(result))
+    .catch(err => res.status(500).json(err))
+})
+
 module.exports = router;
