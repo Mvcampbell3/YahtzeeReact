fcbf7b1b4cd0c64a93df5ecd548a69221f1e1d64
Mvcampbell3 <mvcampbell3@gmail.com>
2021-06-11 20:58:58 -0400

    client npm update and mongo update

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const routes = require("./routes")
+const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require("mongoose");
@@ -9,16 +9,16 @@ const runRemoveAll = require("./scripts/remove");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('dotenv').config()
+require("dotenv").config();
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+    app.use(express.static("client/build"));
 }
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.use(routes)
+app.use(routes);
 
 // Change DB name here
 const dbase = "yahtzee";
@@ -26,21 +26,24 @@ const addSeed = false;
 const emptyDatabase = false;
 
 mongoose
-  .connect(process.env.MONGODB_URI || `mongodb://localhost/${dbase}`, { useNewUrlParser: true, useCreateIndex: true })
-  .then(() => {
-    console.log("mongoDB linked");
-    if (addSeed) {
-      runSeed();
-    }
-    if (emptyDatabase) {
-      runRemoveAll();
-    }
-  })
-  .catch(err => {
-    console.log(err)
-  })
+    .connect(process.env.MONGODB_URI || `mongodb://localhost/${dbase}`, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("mongoDB linked");
+        if (addSeed) {
+            runSeed();
+        }
+        if (emptyDatabase) {
+            runRemoveAll();
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
-
-app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+app.listen(PORT, function () {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
